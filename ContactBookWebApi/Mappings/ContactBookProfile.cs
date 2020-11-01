1c6d74ea85fdf67d6fe2fd9b4a39a708be7e284c
Junior Matlou <junianohmm6@gmail.com>
2020-11-01 20:49:01 +0200

    Adds Profiles for Mapping.

--- ContactBookWebApi/Mappings/ContactBookProfile.cs
+++ ContactBookWebApi/Mappings/ContactBookProfile.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using AutoMapper;
+using ContactBookWebApi.Entities;
+using ContactBookWebApi.Mappings.DTOs.Contact;
+using ContactBookWebApi.Mappings.DTOs.ContactDetail;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ContactBookWebApi.Mappings
 {
-    public class ContactBookProfile
+    public class ContactBookProfile : Profile
     {
+        public ContactBookProfile()
+        {
+            CreateMap<Contact, ContactReadDto>();
+            CreateMap<ContactReadDto, Contact>();
+            CreateMap<ContactCreateDto, Contact>();
+
+            CreateMap<ContactDetail, ContactDetailReadDto>();
+            CreateMap<ContactDetailReadDto, ContactDetail>();
+            CreateMap<ContactDetailCreateDto, ContactDetail>();
+        }
     }
 }
